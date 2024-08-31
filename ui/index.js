9c9f553ed2eb67c9d5b3fb8d0b912e08486097a2
Jeremías <jrmsdev@gmail.com>
2024-08-31 00:02:45 -0300

    ui load go wasm module

--- ui/index.js
+++ ui/index.js
@@ -2,5 +2,14 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
 
-const root = createRoot(document.getElementById('root'));
-root.render(<App />);
+import './static/pkg/wasm_exec.js';
+
+const go = new Go();
+WebAssembly.instantiateStreaming(fetch('./pkg/anonchess.wasm'), go.importObject).then((result) => {
+
+	go.run(result.instance);
+
+	const root = createRoot(document.getElementById('root'));
+	root.render(<App />);
+
+});
