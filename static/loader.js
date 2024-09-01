5d166db3cc9686fa0c69c5844163bc9d2d631883
Jeremías <jrmsdev@gmail.com>
2024-09-01 20:39:31 -0300

    home assets loader

---
+++ static/loader.js
@@ -0,0 +1,16 @@
+fetch('./pkg/anonchess-play.wasm');
+
+fetch('./lila/public/images/board/wood4.jpg');
+
+fetch('./lila/public/piece/cburnett/wK.svg');
+fetch('./lila/public/piece/cburnett/bQ.svg');
+fetch('./lila/public/piece/cburnett/wP.svg');
+fetch('./lila/public/piece/cburnett/wN.svg');
+fetch('./lila/public/piece/cburnett/bP.svg');
+fetch('./lila/public/piece/cburnett/wQ.svg');
+fetch('./lila/public/piece/cburnett/bN.svg');
+fetch('./lila/public/piece/cburnett/bB.svg');
+fetch('./lila/public/piece/cburnett/bK.svg');
+fetch('./lila/public/piece/cburnett/wB.svg');
+fetch('./lila/public/piece/cburnett/bR.svg');
+fetch('./lila/public/piece/cburnett/wR.svg');
