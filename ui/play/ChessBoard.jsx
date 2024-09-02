327aeb64cd1834110402dd50aef814e88b1ad8b1
Jeremías <jrmsdev@gmail.com>
2024-09-01 22:53:36 -0300

    board name squares

--- ui/play/ChessBoard.jsx
+++ ui/play/ChessBoard.jsx
@@ -14,13 +14,18 @@ function ChessBoard() {
 		'Q': 'wQ.svg', 'K': 'wK.svg', 'P': 'wP.svg',
 	};
 
+	const files = 'abcdefgh';
+	const ranks = '12345678';
+
 	const renderBoard = () => {
+		const squareName = "";
 		return board.map((row, rowIndex) => (
 			<div className="chess-row" key={rowIndex}>
 				{row.split('').map((square, colIndex) => (
-					<div className="chess-square" key={colIndex}>
+					<div className="chess-square" key={colIndex} data-square={`${files[colIndex]}${ranks[7 - rowIndex]}`}>
 						{square !== ' ' && (
 							<img
+								data-square={`${files[colIndex]}${ranks[7 - rowIndex]}`}
 								src={`../lila/public/piece/cburnett/${pieceToFile[square]}`}
 								alt=""
 								className="chess-piece"
@@ -32,8 +37,20 @@ function ChessBoard() {
 		));
 	};
 
+	const handleClick = (event) => {
+		const square = event.target;
+		//~ console.log(square);
+		//~ console.log(square.attributes);
+		//~ console.log(square.tagName);
+		//~ console.log(square.id);
+		//~ console.log(square.className);
+		//~ console.log(square.textContent);
+		//~ console.log(square.innerHTML);
+		console.log(square.dataset.square);
+	};
+
 	return (
-		<div className="chessboard-container">
+		<div className="chessboard-container" onClick={handleClick}>
 			<img src="../lila/public/images/board/wood4.jpg" alt="Chessboard" className="chessboard-image" />
 			<div className="pieces-overlay">
 				{renderBoard()}
