789568134c162e262b7a6e78810c35994346079a
Jeremías <jrmsdev@gmail.com>
2024-09-02 22:46:19 -0300

    sidebar layout

--- ui/play/Play.jsx
+++ ui/play/Play.jsx
@@ -2,12 +2,21 @@
 // See LICENSE file.
 
 import React from 'react';
+
 import ChessBoard from './ChessBoard';
+import SideBar    from './SideBar';
 
 function Play() {
 	return (
 		<div className="w3-container">
-			<ChessBoard />
+			<div className="w3-row">
+				<div className="w3-col s1 m1 l1">
+					<SideBar />
+				</div>
+				<div className="w3-col s11 m11 l11">
+					<ChessBoard />
+				</div>
+			</div>
 		</div>
 	);
 }
---
+++ ui/play/SideBar.jsx
@@ -0,0 +1,13 @@
+// Copyright Jeremías Casteglione <jrmsdev@gmail.com>
+// See LICENSE file.
+
+import React from 'react';
+
+function SideBar() {
+	return (
+		<div className="w3-container">
+			<p>Faking</p>
+		</div>
+	);
+}
+export default SideBar;
