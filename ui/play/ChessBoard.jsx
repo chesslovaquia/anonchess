d99e9583cf66c49f622244a5831dfb6f556b85f7
Jeremías <jrmsdev@gmail.com>
2024-09-04 21:01:00 -0300

    relocate render board code

--- ui/play/ChessBoard.jsx
+++ ui/play/ChessBoard.jsx
@@ -3,8 +3,82 @@
 
 import React from 'react';
 
-import { renderBoard, handleMove } from '../game.js';
+import { handleMove } from '../game.js';
 
+//
+// saveGame
+//
+function saveGame() {
+	sessionStorage.setItem('anonc_game', anonc_game_dump());
+}
+
+//
+// loadGame
+//
+function loadGame() {
+	const g = sessionStorage.getItem('anonc_game');
+	if (g) {
+		return anonc_game_load(g);
+	}
+	return null
+}
+
+//
+// renderBoard
+//
+function renderBoard(renderReg) {
+	console.log('renderBoard:', anonc_board());
+
+	let board = loadGame();
+	if (!board) {
+		board = anonc_board_map();
+	}
+
+	const pieceImage = {
+		'r': 'bR.svg', 'n': 'bN.svg', 'b': 'bB.svg',
+		'q': 'bQ.svg', 'k': 'bK.svg', 'p': 'bP.svg',
+		'R': 'wR.svg', 'N': 'wN.svg', 'B': 'wB.svg',
+		'Q': 'wQ.svg', 'K': 'wK.svg', 'P': 'wP.svg',
+	};
+
+	const pieceAlt = {
+		'r': '♜', 'n': '♞', 'b': '♝', 'q': '♛', 'k': '♚', 'p': '♟',
+		'R': '♖', 'N': '♘', 'B': '♗', 'Q': '♕', 'K': '♔', 'P': '♙',
+	};
+
+	const files = 'abcdefgh';
+	const ranks = '12345678';
+
+	const sq = (file, rank) => {
+		return `${files[file]}${ranks[7 - rank]}`;
+	};
+
+	const sqid = (file, rank) => {
+		return `square-${files[file]}${ranks[7 - rank]}`;
+	};
+
+	return board.map((row, rowIndex) => (
+		<div className="chess-row" key={rowIndex}>
+			{row.split('').map((square, colIndex) => (
+				<div className="chess-square" key={colIndex} id={sqid(colIndex, rowIndex)} data-square={sq(colIndex, rowIndex)} data-kind="square">
+					{square !== ' ' && (
+						<img
+							data-kind="piece"
+							data-square={sq(colIndex, rowIndex)}
+							src={`../lila/public/piece/cburnett/${pieceImage[square]}`}
+							alt={`${pieceAlt[square]}`}
+							className="chess-piece"
+						/>
+					)}
+				</div>
+			))}
+		</div>
+	));
+};
+
+//
+// ChessBoard
+//
 function ChessBoard({renderReg}) {
 	const highlight = '2px solid yellow';
 
