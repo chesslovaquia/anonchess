d4b41c904356be3ddc3b4b5e869d4ae46253b356
Jeremías <jrmsdev@gmail.com>
2024-10-19 00:53:55 -0300

    game: test valid moves

--- lib/game/game_test.go
+++ lib/game/game_test.go
@@ -7,6 +7,7 @@ import (
 	"testing"
 
 	"hash/fnv"
+	"strings"
 )
 
 const pos = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1"
@@ -123,4 +124,14 @@ func TestBoardMap(t *testing.T) {
 		"        ",
 		"        ",
 	})
+}
+
+func TestValidMoves(t *testing.T) {
+	initBoard(t)
+	vm := ValidMoves()
+	got := strings.Join(vm, " ")
+	expect := "b1a3 b1c3 g1f3 g1h3 a2a3 a2a4 b2b3 b2b4 c2c3 c2c4 d2d3 d2d4 e2e3 e2e4 f2f3 f2f4 g2g3 g2g4 h2h3 h2h4"
+	if got != expect {
+		t.Errorf("ValidMoves Error\ngot:    '%s'\nexpect: '%s'", got, expect)
+	}
 }
